d056e1ec92d93e63ad15fcf3369da3674abb653d
Varad Rampurikar <rampurikarvarad@gmail.com>
2023-07-25 19:00:05 +0530

    edited the name change for template in

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin(
             {
-                template: "./src/index.html",
+                template: "./src/template.html",
                 favicon: "./src/assets/favicon/pizza.png"
             }
         )
